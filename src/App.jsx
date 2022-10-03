c52a279abd450c18a7b0dddebf3af8d4fb5f6e97
Gavin Stirling <gavin.stirling@longview-partners.com>
2022-10-03 16:39:30 +0100

    Rearranged homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
 import "./App.css";
 
-import { useState } from "react";
-
-import NavBar from "./containers/NavBar/NavBar";
-import Library from "./containers/Library/Library";
-import Explore from "./containers/Explore/Explore";
-import SearchBar from "./components/SearchBar/SearchBar";
-
-import userLibrary from "./assets/data/dummydata";
+import Home from "./containers/Home/Home";
 
 const App = () => {
-  const [userLibraryArray, setUserLibraryArray] = useState(userLibrary);
+
 
   return (
     <div className="App">
-      <NavBar />
-      My Library
-      <div className="searchbars">
-        <SearchBar search_item="Book Name" />
-        <SearchBar search_item="ISBN" />
-      </div>
-      <div className="containers">
-        <Library libraryArray={userLibraryArray} />
-        <Explore />
-      </div>
+      <Home />
     </div>
   );
 };
