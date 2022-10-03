3575a8ba0517dc5fa017d573c0d35524bdda9d68
Gavin Stirling <gavin.stirling@longview-partners.com>
2022-10-03 15:16:28 +0100

    Created search bars

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   return (
     <div className="App">
       My Library
-      <SearchBar />
+      <SearchBar search_item="Book Name" />
+      <SearchBar search_item="ISBN" />
       <Library />
       <Explore />
       <BookCard />
