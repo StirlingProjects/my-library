75ca52af31ed53a44c3535f2418a034f90cbc145
Gavin Stirling <gavin.stirling@longview-partners.com>
2022-10-04 11:39:49 +0100

    Added basic book cards

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -7,13 +7,14 @@ const BookCard = (props) => {
   const isbn = Object.keys(book)[0];
   const details = book[isbn].details;
   const title = details.title;
-  const author = details.title
+  const author = details.authors[0].name;
 
   return (
     <div className="bookcard">
       <div>
-        <span>Title: </span> <span>{title}</span><br />
-        <span>Title: </span> <span>{author}</span>
+        <span>Title: </span> <span>{title}</span>
+        <br />
+        <span>Author: </span> <span>{author}</span>
       </div>
     </div>
   );
